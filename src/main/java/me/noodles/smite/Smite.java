12b2410a6b694008d73a7310e22b04e12f36db64
thinkverse <ctuvedsson@gmail.com>
2020-04-11 16:58:23 +0200

    Refactor /smite command

--- src/main/java/me/noodles/smite/Smite.java
+++ src/main/java/me/noodles/smite/Smite.java
@@ -1,12 +1,12 @@
 package me.noodles.smite;
 
+import me.noodles.smite.commands.SmiteCommand;
 import me.noodles.smite.listeners.UpdateJoinEvent;
 import me.noodles.smite.utilities.UpdateChecker;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.*;
 import org.bukkit.command.*;
-import org.bukkit.entity.*;
 import org.bukkit.event.Listener;
 import org.bukkit.*;
 
@@ -22,6 +22,7 @@ public class Smite extends JavaPlugin implements Listener
         this.getLogger().info("Smite V" + VarUtilType.getVersion() + " starting...");
         this.saveDefaultConfig();
         this.reloadConfig();
+        this.registerCommand("smite", new SmiteCommand());
         registerEvents((Plugin)this, new UpdateJoinEvent(this));
         registerEvents(this, this);
         this.getLogger().info("Smite V" + VarUtilType.getVersion() + " started!");
@@ -47,46 +48,14 @@ public class Smite extends JavaPlugin implements Listener
             Bukkit.getServer().getPluginManager().registerEvents(listener, plugin);
         }
     }
+
+    private void registerCommand(final String command, final CommandExecutor executor) {
+        this.getCommand(command).setExecutor(executor);
+    }
     
     @SuppressWarnings({ "unchecked", "rawtypes"})
 	public static Smite getPlugin() {
         return (Smite)getPlugin((Class) Smite.class);
     }
-    
 
-    @SuppressWarnings("unused")
-	public boolean onCommand(final CommandSender sender, final Command cmd, final String commandLabel, final String[] args) {
-    	if (!(sender instanceof Player)){
-            Bukkit.getServer().getLogger().info("Only players can do this!");
-            return true;
-    		}
-    	final Player p = (Player)sender;
-        if (!cmd.getName().equalsIgnoreCase("smite")) {
-            return true;
-        }
-        if (args.length == 0) {
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cInvalid! Use like /smite playersname!"));
-            return true;
-        }
-        if (args.length > 1) {
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cInvalid! Use like /smite playersname!"));
-            return true;
-        }
-        else {
-            final Player target = Bukkit.getServer().getPlayer(args[0]);
-            if (target == null) {
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cInvalid! Player not found!"));
-                return true;
-            }
-            if (!sender.hasPermission("smite.use")) {
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "(!) You don't have permssion to use this command!"));
-                return true;
-            }
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&aHit " + ChatColor.YELLOW  + target.getName() + " &awith a lightning strike!"));
-            final Location i = target.getLocation();
-            final World world2 = target.getWorld();
-            world2.strikeLightningEffect(i);
-            return true;
-        }
-    }
 }
