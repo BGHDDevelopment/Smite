95f1c30dce78d111db68e0b76fb72dc7eb1fe45c
RyanMoodGAMING <ryan@ryanmood.com>
2022-12-29 21:51:57 +0000

    Update update checker

--- src/main/java/com/bghddevelopment/smite/Smite.java
+++ src/main/java/com/bghddevelopment/smite/Smite.java
@@ -1,13 +1,20 @@
 package com.bghddevelopment.smite;
 
 import com.bghddevelopment.smite.commands.SmiteCommand;
-import com.bghddevelopment.smite.listeners.UpdateJoinEvent;
-import com.bghddevelopment.smite.utilities.UpdateChecker;
+import com.bghddevelopment.smite.utilities.Color;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Smite extends JavaPlugin {
 
@@ -25,32 +32,67 @@ public class Smite extends JavaPlugin {
 
         this.getLogger().info(String.format("Smite v%s loading events ...", version));
 
-        this.registerEvents(this, new UpdateJoinEvent(this));
-
         this.getLogger().info(String.format("Smite v%s started ...", version));
 
-        if (getConfig().getBoolean("CheckForUpdates.Enabled", true)) {
-            new UpdateChecker(this, 46604).getLatestVersion(remoteVersion -> {
-                getLogger().info("Checking for Updates ...");
-
-                if (getDescription().getVersion().equalsIgnoreCase(remoteVersion)) {
-                    getLogger().info("No new version available");
-                } else {
-                    getLogger().warning(String.format("Newest version: %s is out! You are running version: %s", remoteVersion, getDescription().getVersion()));
-                    getLogger().warning("Please Update Here: http://www.spigotmc.org/resources/46604");
-                }
-            });
-        }
-    }
-
-    private void registerEvents(final Plugin plugin, final Listener... listeners) {
-        for (final Listener listener : listeners) {
-            Bukkit.getServer().getPluginManager().registerEvents(listener, plugin);
-        }
+        updateCheck(Bukkit.getConsoleSender(), true);
     }
 
     private void registerCommand(final String command, final CommandExecutor executor) {
         this.getCommand(command).setExecutor(executor);
+    }
+
+    public void updateCheck(CommandSender sender, boolean console) {
+        try {
+            String urlString = "https://updatecheck.bghddevelopment.com";
+            URL url = new URL(urlString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("User-Agent", "Mozilla/5.0");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String input;
+            StringBuffer response = new StringBuffer();
+            while ((input = reader.readLine()) != null) {
+                response.append(input);
+            }
+            reader.close();
+            JsonObject object = new JsonParser().parse(response.toString()).getAsJsonObject();
+
+            if (object.has("plugins")) {
+                JsonObject plugins = object.get("plugins").getAsJsonObject();
+                JsonObject info = plugins.get("Smite").getAsJsonObject();
+                String version = info.get("version").getAsString();
+                Boolean archived = info.get("archived").getAsBoolean();
+                if (archived) {
+                    sender.sendMessage(Color.translate(""));
+                    sender.sendMessage(Color.translate(""));
+                    sender.sendMessage(Color.translate("&cThis plugin has been marked as 'Archived' by BGHDDevelopment LLC."));
+                    sender.sendMessage(Color.translate("&cThis version will continue to work but will not receive updates or support."));
+                    sender.sendMessage(Color.translate(""));
+                    sender.sendMessage(Color.translate(""));
+                    return;
+                }
+                if (version.equals(getDescription().getVersion())) {
+                    if (console) {
+                        sender.sendMessage(Color.translate("&aSmite is on the latest version."));
+                    }
+                } else {
+                    sender.sendMessage(Color.translate(""));
+                    sender.sendMessage(Color.translate(""));
+                    sender.sendMessage(Color.translate("&cYour Smite version is out of date!"));
+                    sender.sendMessage(Color.translate("&cWe recommend updating ASAP!"));
+                    sender.sendMessage(Color.translate(""));
+                    sender.sendMessage(Color.translate("&cYour Version: &e" + getDescription().getVersion()));
+                    sender.sendMessage(Color.translate("&aNewest Version: &e" + version));
+                    sender.sendMessage(Color.translate(""));
+                    sender.sendMessage(Color.translate(""));
+                }
+            } else {
+                sender.sendMessage(Color.translate("&cWrong response from update API, contact plugin developer!"));
+            }
+        } catch (
+                Exception ex) {
+            sender.sendMessage(Color.translate("&cFailed to get updater check. (" + ex.getMessage() + ")"));
+        }
     }
 
 }
